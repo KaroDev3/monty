ce7f69cb2acd4bedd5f9c64386e602de948fd6e6
KaroDev3 <karodev3@gmail.com>
2020-05-14 19:03:30 +0000

    betty style in 'f_pstr.c'

--- f_pstr.c
+++ f_pstr.c
@@ -30,17 +30,3 @@ void f_pstr(stack_t **stack, unsigned int line_number)
 	}
 	printf("\n");
 }
-
-/*
-
-The opcode pstr prints the string starting at the top of the stack, followed by a new line.
-
-    Usage: pstr
-    The integer stored in each element of the stack is treated as the ascii value of the character to be printed
-    The string stops when either:
-        the stack is over
-        the value of the element is 0
-        the value of the element is not in the ascii table
-    If the stack is empty, print only a new line
-
-*/
